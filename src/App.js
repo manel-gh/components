ebb8be1089a1cfdc5b2bfd9cb98a1338376b25ea
manel gharbi <manelgharbi23@gmail.com>
2021-08-20 12:24:18 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import Products from './Components/Products/Products';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Products/>
+    </div>
+  );
+}
+
+export default App;
